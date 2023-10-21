fb0ca2d9bf3d930864eb0dfb58eefb261beaf8f0
Jesus Briones <leojesusbriones2@gmail.com>
2023-10-21 16:33:36 -0500

    baseweb script finished

--- baseweb.bat
+++ baseweb.bat
@@ -1,40 +1,68 @@
 @echo off
-setlocal enabledelayedexpansion
-
-echo "hola"
 
 :: Nombre de la carpeta y archivos
-:: set carpeta="mi_carpeta"
-:: set html_file="%carpeta%\index.html"
-:: set css_file="%carpeta%\styles.css"
-:: set js_file="%carpeta%\script.js"
+set folder=%~1
+set html_file="%folder%\index.html"
+set css_file="%folder%\styles.css"
+set js_file="%folder%\script.js"
 
-:: Crear la carpeta
-:: mkdir %carpeta%
+:: Comprobar si se proporciona un nombre de carpeta como argumento
+if "%~1"=="" (
+  echo Se require especificar el nombre del proyecto.
+  goto end  
+)
+
+:: Comprobar si la carpeta ya existe
+if exist "%folder%" (
+  echo La carpeta "%folder%" ya existe.
+  echo.
+  goto end
+) else (
+  mkdir "%folder%"
+)
+
 
 :: Crear el archivo HTML y agregar la estructura base
-:: echo ^<!DOCTYPE html^> > %html_file%
-:: echo ^<html^> >> %html_file%
-:: echo. >> %html_file%
-:: echo ^<head^> >> %html_file%
-:: echo   ^<link rel="stylesheet" type="text/css" href="styles.css"^> >> %html_file%
-:: echo   ^<script type="text/javascript" src="script.js"^>^</script^> >> %html_file%
-:: echo ^</head^> >> %html_file%
-:: echo. >> %html_file%
-:: echo ^<body^> >> %html_file%
-:: echo   <h1>Mi página web</h1> >> %html_file%
-:: echo ^</body^> >> %html_file%
-:: echo. >> %html_file%
-:: echo ^</html^> >> %html_file%
+(
+  echo ^<!DOCTYPE html^>
+  echo ^<html lang="es"^>
+  echo ^<head^>
+  echo   ^<meta charset="UTF-8"^>
+  echo   ^<meta name="viewport" content="width=device-width, initial-scale=1.0"^>
+  echo   ^<title^>base web^</title^>
+  echo   ^<link rel="stylesheet" href="./styles.css"^>
+  echo   ^<script defer src="./script.js"^>^</script^>
+  echo ^</head^>
+  echo ^<body^>
+  echo.
+  echo.   
+  echo.
+  echo ^</body^>
+  echo ^</html^>
+) > %html_file%
+
 
 :: Crear el archivo CSS
-:: echo body { >> %css_file%
-:: echo   font-family: Arial, sans-serif; >> %css_file%
-:: echo   background-color: #f0f0f0; >> %css_file%
-:: echo } >> %css_file%
+(
+  echo * {
+  echo   margin: 0;
+  echo   padding: 0;
+  echo   box-sizing: border-box;
+  echo }
+  echo.
+  echo body {
+  echo   font-family: sans-serif;
+  echo }
+  echo.
+) > %css_file%
 
 :: Crear el archivo JavaScript
-:: echo console.log("Hola desde JavaScript"); >> %js_file%
+echo. > %js_file%
 
-:: echo Archivos creados en la carpeta %carpeta%
-:: pause
+
+echo Se creo la carpeta %folder% con tres archivos:
+echo - index.html
+echo - styles.css
+echo - script.js
+echo.
+:end
