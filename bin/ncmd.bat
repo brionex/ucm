4a190e58e5ba0c70c2c7330f708e5a9c032abc52
leonel <leojesusbriones2@gmail.com>
2024-03-26 17:06:46 -0500

    update

--- bin/ncmd.bat
+++ bin/ncmd.bat
@@ -1,6 +1,5 @@
 @echo off
 set "dirname=%~dp0"
 set "venvpy=%dirname%..\.venv\Scripts\python.exe"
-
 set PYTHONPATH=%dirname%\modules;%PYTHONPATH%
-%venvpy% "%dirname%..\data\newcmd\newcmd.py" %*
+%venvpy% "%dirname%..\data\ncmd\ncmd.py" %*
--- modules/lib.py
+++ modules/lib.py
@@ -4,7 +4,3 @@ import re
 def validate_folder_name(name):
     pattern = r'^[a-zA-Z0-9_-]+$'
     return re.match(pattern, name) is not None
-
-
-def saludar():
-    print('hola desde los módulos')
