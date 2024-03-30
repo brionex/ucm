9bd875b48015910e812bf7a6345ed5d587da6a55
leonel <leojesusbriones2@gmail.com>
2024-03-30 14:53:32 -0500

    implementacion y refactorizacion del comando 'ucmd'

--- data/ucmd/ucmd.py
+++ data/ucmd/ucmd.py
@@ -1,98 +1,86 @@
 import os
 import shutil
+from pathlib import Path
 import click
+from const import CONST
 
 
-# Obtiene la ruta del directorio actual
-current_dir = os.getcwd()
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BIN_DIR = os.path.join(BASE_DIR, 'bin')
+USER_DIR = os.path.join(BASE_DIR, 'user')
 
 
-
-@click.group()
+@click.group(help=CONST.ucmd)
 def ucmd():
-    """Gestiona los comandos creados por el usuario"""
+    pass
 
 
-@ucmd.command()
+@ucmd.command(help=CONST.ls)
 def ls():
-    """Lista todos los comandos disponibles"""
-    cmd_folder = os.path.join(current_dir, 'bin')
-    cmd_list = os.listdir(cmd_folder)
-    max_len = len(max(cmd_list, key=len)) - 4
+    cmds_list = os.listdir(BIN_DIR)
+    max_len = len(max(cmds_list, key=len)) - 4
 
     print()
-    for path in cmd_list:
-        name, extension = os.path.splitext(path)
-        if extension == '.bat':
-            with open(os.path.join(cmd_folder, path), "r", encoding="utf-8") as f:
-                linea = f.readline()
-                print(f'  {name.ljust(max_len, ' ')} {
-                      linea.replace('::', '')}', end='')
+    for file_name in cmds_list:
+        name, extension = os.path.splitext(file_name)
+
+        if not extension == '.bat':
+            continue
+
+        # Lee el archivo bat para obtener la descripción del comando
+        with open(os.path.join(BIN_DIR, file_name), "r", encoding="utf-8") as f:
+            line = f.readline()
+
+        print(f'  {name.ljust(max_len, ' ')} {line.replace('::', '')}', end='')
     print()
 
 
-@ucmd.command()
+@ucmd.command(help=CONST.add)
 @click.argument('name')
-@click.option('-m', '--message', required=True, help='Agrega una descripción al comando')
+@click.option('-m', '--message', required=True, help=CONST.add_op)
 def add(name, message):
-    """Crea los archivos para un nuevo comando"""
-
     # Crea la carpeta para los comandos del usuario
-    user_folder = os.path.join(current_dir, 'user')
-    os.makedirs(user_folder, exist_ok=True)
+    os.makedirs(USER_DIR, exist_ok=True)
 
-    # Crea los archivos para implementar el comando
-    command_bat = os.path.join(current_dir, 'bin', f'{name}.bat')
-    command_folder = os.path.join(user_folder, name)
-    command_py = os.path.join(user_folder, name, f'{name}.py')
+    bat_file = os.path.join(BIN_DIR, f'{name}.bat')
+    py_file = os.path.join(USER_DIR, name, f'{name}.py')
 
     # Verifica si ya existe el comando que se quiere crear
-    if os.path.exists(command_bat):
-        print('\n  !Ya existe un comando con ese nombre\n')
+    if os.path.exists(bat_file):
+        print(CONST.cmd_exist)
         return
 
-    # Crea el archivo bat.
-    content_bat = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)),
-        'content.txt'
-    )
-
+    # Crea el archivo bat
+    content_bat = os.path.join(Path(__file__).resolve().parent, 'content.txt')
     with open(content_bat, 'r', encoding="utf-8") as f:
-        content = f.read().format(message, name)
+        lines = f.read().format(message, name)
+    with open(bat_file, 'w', encoding="utf-8") as f:
+        f.write(lines)
 
-    with open(command_bat, 'w', encoding="utf-8") as f:
-        f.write(content)
-
-    # Crea la carpeta y el archivo python principal.
-    os.makedirs(command_folder, exist_ok=True)
-    with open(command_py, 'w', encoding="utf-8") as f:
-        f.write(f'print("Ejecución del archivo {name}.py")')
-
-    print(f'\n  Comando "{name}" creado con éxito.\n')
+    # Crea la carpeta y el archivo python principal
+    os.makedirs(os.path.join(USER_DIR, name), exist_ok=True)
+    with open(py_file, 'w', encoding="utf-8") as f:
+        f.write(CONST.py_content.format(name))
+    print(CONST.cmd_created.format(name))
 
 
-@ucmd.command()
+@ucmd.command(help=CONST.remove)
 @click.argument('name')
 def remove(name):
-    """Elimina un comando creado por el usuario"""
+    bat_file = os.path.join(BIN_DIR, f'{name}.bat')
+    data_folder = os.path.join(USER_DIR, name)
 
-    bat_file = os.path.join(current_dir, 'bin', f'{name}.bat')
-    data_folder = os.path.join(current_dir, 'user', name)
-
-    exists_file = os.path.exists(bat_file)
-    exists_folder = os.path.exists(data_folder)
-
-    if exists_file and exists_folder:
-        res = input(f'  Confirmar eliminar comando "{name}" (s): ')
-        if res.lower() == 's':
-            os.remove(bat_file)
-            shutil.rmtree(data_folder)
-            print('\n  !Comando eliminado con éxito\n')
-        else:
-            print('\n  Eliminación cancelada\n')
-    else:
-        print('\n  !El comando que deseas eliminar no existe\n')
+    if not (os.path.exists(bat_file) and os.path.exists(data_folder)):
+        print(CONST.cmd_not_exist)
         return
+    
+    res = input(CONST.confirm_remove.format(name))
+    if res.lower() == 's':
+        os.remove(bat_file)
+        shutil.rmtree(data_folder)
+        print(CONST.cmd_removed)
+    else:
+        print(CONST.remove_cancel)
 
 
 if __name__ == "__main__":
